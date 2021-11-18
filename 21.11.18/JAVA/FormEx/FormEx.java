89a074b6dc15eb5f5caeca3df451db71e24704e2
BigMiniKim <91724500+BigMiniKim@users.noreply.github.com>
2021-11-18 17:08:16 +0900

    Create FormEx.java

---
+++ 21.11.18/JAVA/FormEx/FormEx.java
@@ -0,0 +1,60 @@
+package edu.kosmo.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/formEx") // 
+public class FormEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		System.out.println(id);
+		System.out.println(pw);
+		
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>"); //writer로 연결. 계속 해제.
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println(id);
+		writer.println(pw);
+		writer.println("<body>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		System.out.println("포스트 호출");
+		
+	}
+	
+
+}
