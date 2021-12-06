92a2d85b828ff9c011959631b0457486050b6833
BigMiniKim <91724500+BigMiniKim@users.noreply.github.com>
2021-12-06 11:34:03 +0900

    Create BCommand.java

---
+++ 21.12.06/Board/BCommand.java
@@ -0,0 +1,12 @@
+package edu.kosmo.ex.command;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface BCommand {
+	
+	void execute(HttpServletRequest request, HttpServletResponse response); //자손이 구현해야함
+	
+}
+
