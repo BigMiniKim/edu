87f1862f329cb95b13a2600989bf3c868bf68a34
BigMiniKim <91724500+BigMiniKim@users.noreply.github.com>
2021-12-06 11:34:35 +0900

    Create BController.java

---
+++ 21.12.06/Board/BController.java
@@ -0,0 +1,80 @@
+package edu.kosmo.ex.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.kosmo.ex.command.BCommand;
+import edu.kosmo.ex.command.BContentCommand;
+import edu.kosmo.ex.command.BDeleteCommand;
+import edu.kosmo.ex.command.BListCommand;
+import edu.kosmo.ex.command.BModifyCommand;
+import edu.kosmo.ex.command.BReplyCommand;
+import edu.kosmo.ex.command.BReplyViewCommand;
+import edu.kosmo.ex.command.BWriteCommand;
+
+/**
+ * Servlet implementation class BoardFrontController
+ */
+@WebServlet("*.do")
+public class BController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("actionDo");
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		String viewPage = null;
+		BCommand command = null;
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		
+		System.out.println(uri);
+		System.out.println(conPath);
+		System.out.println(com);
+		
+		
+		if(com.equals("/list.do")) {
+			command = new BListCommand();
+			command.execute(request, response);
+			viewPage = "list.jsp";
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage); //request객체에다가 viewPage를 설정하면 유저에게 viewPag?list.jsp?를 보여줌.
+		dispatcher.forward(request, response);//request, response 객체를 list.jsp에다가 forwarding
+	}
+}
