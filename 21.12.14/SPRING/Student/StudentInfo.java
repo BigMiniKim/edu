c9d40b06593753ad5603ff7032e84a5d1fb07027
BigMiniKim <91724500+BigMiniKim@users.noreply.github.com>
2021-12-14 12:23:43 +0900

    Add files via upload

---
+++ 21.12.14/SPRING/Student/StudentInfo.java
@@ -0,0 +1,17 @@
+package com.javalec.ex.student;
+
+public class StudentInfo {
+	private Student student;
+
+	private StudentInfo() {
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+}
