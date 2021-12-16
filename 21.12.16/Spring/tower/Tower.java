f00345244983b76585511b5df701fdc30213e53e
BigMiniKim <91724500+BigMiniKim@users.noreply.github.com>
2021-12-16 17:34:01 +0900

    Create Tower.java

---
+++ 21.12.16/Spring/tower/Tower.java
@@ -0,0 +1,21 @@
+package edu.kosmo.ex;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Data
+public class Tower {
+	public int stage;
+
+	
+       }
+
