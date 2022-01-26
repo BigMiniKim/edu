254bef01c5ca7fc232bcae75853c599cc3c70b64
BigMiniKim <91724500+BigMiniKim@users.noreply.github.com>
2022-01-26 18:41:37 +0900

    Add files via upload

---
+++ 22.01.26/InputGrade.js
@@ -0,0 +1,15 @@
+
+
+/*객체구조분할*/
+const InputGrade = ({sub}) => {
+    console.log(sub);
+
+    return (<div>
+        <label>{sub} 
+        <input type="text">
+            </input></label>
+    </div>);
+
+
+};
+export default InputGrade ;
